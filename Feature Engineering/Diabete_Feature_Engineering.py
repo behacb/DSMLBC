86be5a4dd7c2ba88b648a5fba4b15dddfbc73610
Bedirhan Hasançebi <102032422+behacb@users.noreply.github.com>
2023-04-06 22:58:50 +0300

    Update Diabete_Feature_Engineering.py

--- Feature Engineering/Diabete_Feature_Engineering.py
+++ Feature Engineering/Diabete_Feature_Engineering.py
@@ -34,7 +34,7 @@ pd.set_option('display.max_rows', 20)
 pd.set_option('display.float_format', lambda x: '%.3f' % x)
 
 
-df = pd.read_csv("C:\\Users\\bedir\\Desktop\\bedoli\\DSMLBC\\feature_engineering\\odev\\diabetes\\diabetes.csv")
+df = pd.read_csv("diabetes\diabetes.csv")
 df.head()
 
 
